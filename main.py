ac3dcc602130e6597149c672ad48bfdead40e3ab
krishna-vinci <111030181+krishna-vinci@users.noreply.github.com>
2023-06-18 23:56:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import os
 import base64
 
-# Function to download data as csv
 def get_csv_download_link(df, filename):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
@@ -14,22 +13,17 @@ def get_csv_download_link(df, filename):
 def generate_password(length, upper, lower, numeric, special_chars):
     password = []
 
-    # Add uppercase letters
     for _ in range(upper):
         password.append(random.choice(string.ascii_uppercase))
 
-    # Add lowercase letters
     for _ in range(lower):
         password.append(random.choice(string.ascii_lowercase))
 
-    # Add numeric characters
     for _ in range(numeric):
         password.append(random.choice(string.digits))
-
-    # Add special characters
+        
     password += list(special_chars)
 
-    # If the password is shorter than the desired length, add random characters
     while len(password) < length:
         char_type = random.choice([0, 1, 2])  # 0 for uppercase, 1 for lowercase, 2 for numeric
 
@@ -44,36 +38,40 @@ def generate_password(length, upper, lower, numeric, special_chars):
 
     return ''.join(password)
 
+st.title("Bulk Password Generator")
 
 
 
+num_passwords = st.number_input('Enter the number of passwords you want to generate', min_value=1, max_value=10, value=1)
 
-website = st.text_input('Enter the name of the website/service')
-length = st.number_input('Enter the maximum length of the password', min_value=1, max_value=100, value=8)
-upper = st.number_input('Enter the number of uppercase letters', min_value=0, max_value=100, value=2)
-lower = st.number_input('Enter the number of lowercase letters', min_value=0, max_value=100, value=2)
-numeric = st.number_input('Enter the number of numeric characters', min_value=0, max_value=100, value=2)
-special_chars = st.text_input('Enter the special characters you want to include')
+data = []
 
-if st.button('Generate Password'):
+for i in range(num_passwords):
+    st.subheader(f'Password {i+1}')
+    name = st.text_input(f'Enter a name/identifier for password {i+1}')
+    length = st.number_input(f'Enter the maximum length of password {i+1}', min_value=1, max_value=100, value=8)
+    upper = st.number_input(f'Enter the number of uppercase letters for password {i+1}', min_value=0, max_value=100, value=2)
+    lower = st.number_input(f'Enter the number of lowercase letters for password {i+1}', min_value=0, max_value=100, value=2)
+    numeric = st.number_input(f'Enter the number of numeric characters for password {i+1}', min_value=0, max_value=100, value=2)
+    special_chars = st.text_input(f'Enter the special characters you want to include in password {i+1}')
+    
     password = generate_password(length, upper, lower, numeric, special_chars)
-    st.text_area("Your password is:", value=password)  # User can manually copy from here
-    st.text("manual copy")
+    data.append([name, password])
 
-    # Save password to CSV
-    df = pd.DataFrame({'Website': [website], 'Password': [password]})
+df = pd.DataFrame(data, columns=['Name', 'Password'])
+
+if st.button('Generate Passwords'):
+    st.dataframe(df)
+
     if not os.path.isfile('passwords.csv'):
         df.to_csv('passwords.csv', index=False)
-    else:  # else it exists so append without writing the header
+    else:
         df.to_csv('passwords.csv', mode='a', header=False, index=False)
 
-    # Download link
-    st.markdown(get_csv_download_link(df, 'password.csv'), unsafe_allow_html=True)
+    st.markdown(get_csv_download_link(df, 'passwords.csv'), unsafe_allow_html=True)
 
-    st.success("Done")
 
 st.text('''Note: 
-1. You can download CSV at the end 2. It will add random upper, lower or numeric characters to reach the maximum password length
-
-''')
-
+1. You can generate up to 10 passwords at a time.
+2. Each password can be customised according to your preference.
+3. Passwords can be downloaded as a CSV file.''')
